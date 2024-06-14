84e8e63b6fafcbe94cdd76e736981e5be5425c51
saadli wissem <saadliwissem88@gmail.com>
2024-06-14 09:40:12 +0100

    prod test

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const outcomRoutes = require("./routes/outcomeRoutes");
-const categoryroutes = require("./routes/categoryRoutes");
 const productRoutes = require("./routes/productRoutes");
 const helmet = require("helmet");
-const categoryModal = require("./models/Category");
 
 const userRoutes = require("./routes/userRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 //const Delivery = require("./models/Delivery");
 const app = express();
-const cloudinary = require("./Cloudinary/cloudinary");
 const bodyParser = require("body-parser");
 
 //cors
 const cors = require("cors");
-const Outcome = require("./models/Outcome");
-const User = require("./models/User");
-const Category = require("./models/Category");
 app.use(cors());
 // Middleware
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -49,8 +42,6 @@ app.use("/auth", userRoutes);
 app.use("/products", productRoutes);
 
 app.use("/dashboard", dashboardRoutes);
-app.use("/outcomes", outcomRoutes);
-app.use("/categories", categoryroutes);
 
 const port = process.env.PORT;
 app.listen(port, () => {
